89b95e48f83ddd39078781985a58e86b9f3d5be7
njay6 <7880345+njay6@users.noreply.github.com>
2018-07-03 17:24:03 +0200

    Create GocadTSurfObjExportExample.java
    added example for OBJ file export

---
+++ triturus/src/org/n52/v3d/triturus/geologic/examples/GocadTSurfObjExportExample.java
@@ -0,0 +1,43 @@
+package org.n52.v3d.triturus.geologic.examples;
+
+import org.n52.v3d.triturus.core.T3dException;
+import org.n52.v3d.triturus.geologic.importers.IoGocadTSurfReader;
+import org.n52.v3d.triturus.gisimplm.GmSimpleTINFeature;
+import org.n52.v3d.triturus.gisimplm.IoTINWriter;
+
+/**
+ * Geologic Toolbox example application: Reads a GOCAD TSurf TIN and writes 
+ * it to an OBJ-File.
+ * 
+ * @author Jatzek
+ */
+public class GocadTSurfObjExportExample
+{
+
+	private final String
+		inFilename = "data/GOCAD/aussrtm_Seligenstadt.ts",
+//		inFilename = "data/GOCAD/Flugsand_Base_cut.ts",
+//		inFilename = "data/simple_test.ts",
+		outFilename = "data/simple_test.obj"; 
+	
+	public static void main(String args[]) {
+		new GocadTSurfToObjExample().run();
+	}
+	
+	public void run() 
+	{ 
+		try {
+			// Read first TSurf model from GOCAD data file...
+			IoGocadTSurfReader reader = new IoGocadTSurfReader();
+			GmSimpleTINFeature surf = reader.read(inFilename).get(0);
+			// ... and generate OBJ output:
+			IoTINWriter writer = new IoTINWriter(IoTINWriter.OBJ);
+			writer.writeToFile(surf, outFilename);
+			System.out.println("Wrote the file \"" + outFilename + "\".");
+		}
+		catch (T3dException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
