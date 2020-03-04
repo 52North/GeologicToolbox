be2301569ea0cb38e77bf46ac47ea392b61ee512
BennoSchmidt <benno.schmidt@hs-bochum.de>
2020-03-04 07:54:52 +0100

    add/update copyright information header

--- triturus/src/main/java/org/n52/v3d/triturus/geologic/examples/ElevationGridToShp.java
+++ triturus/src/main/java/org/n52/v3d/triturus/geologic/examples/ElevationGridToShp.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (C) 2018 52 North Initiative for Geospatial Open Source
+ * Copyright (C) 2020 52North Initiative for Geospatial Open Source
  * Software GmbH
  *
  * This program is free software; you can redistribute it and/or modify it
@@ -18,17 +18,17 @@
  *
  * Therefore the distribution of the program linked with libraries licensed
  * under the aforementioned licenses, is permitted by the copyright holders
- * if the distribution is compliant with both the GNU General Public License
+ * if the distribution is compliant with both the GNU General Public License 
  * version 2 and the aforementioned licenses.
  *
  * This program is distributed in the hope that it will be useful, but
- * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
- * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY 
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License 
  * for more details.
  *
- * Contact: Benno Schmidt and Martin May, 52 North Initiative for Geospatial
- * Open Source Software GmbH, Martin-Luther-King-Weg 24, 48155 Muenster,
- * Germany, info@52north.org
+ * Contact: Benno Schmidt, 52 North Initiative for Geospatial Open Source 
+ * Software GmbH, Martin-Luther-King-Weg 24, 48155 Muenster, Germany, 
+ * b.schmidt@52north.org
  */
 package org.n52.v3d.triturus.geologic.examples;
 
