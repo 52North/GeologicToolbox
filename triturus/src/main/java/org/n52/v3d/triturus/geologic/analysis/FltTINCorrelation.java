e81c0380e7a6199ac173608822aa33dafe78580b
BennoSchmidt <benno.schmidt@hs-bochum.de>
2021-09-03 10:12:33 +0200

    add Max Haverkamps's function to compute correlations between TIN-surfaces

--- triturus/src/main/java/org/n52/v3d/triturus/geologic/analysis/FltTINCorrelation.java
+++ triturus/src/main/java/org/n52/v3d/triturus/geologic/analysis/FltTINCorrelation.java
@@ -33,10 +33,17 @@
 package org.n52.v3d.triturus.geologic.analysis;
 
 import org.n52.v3d.triturus.core.T3dException;
-import org.n52.v3d.triturus.core.T3dNotYetImplException;
 import org.n52.v3d.triturus.core.T3dProcFilter;
+import org.n52.v3d.triturus.gisimplm.FltTIN2ElevationGrid;
+import org.n52.v3d.triturus.gisimplm.GmEnvelope;
+import org.n52.v3d.triturus.gisimplm.GmPoint;
+import org.n52.v3d.triturus.gisimplm.GmSimple2dGridGeometry;
 import org.n52.v3d.triturus.gisimplm.GmSimpleElevationGrid;
 import org.n52.v3d.triturus.gisimplm.GmSimpleTINFeature;
+import org.n52.v3d.triturus.vgis.VgElevationGrid;
+import org.n52.v3d.triturus.vgis.VgEnvelope;
+import org.n52.v3d.triturus.vgis.VgEquidistGrid;
+import org.n52.v3d.triturus.vgis.VgPoint;
 
 /**
  * Computation of the correlation coefficient of two surfaces modeled as TINs
@@ -47,24 +54,27 @@ import org.n52.v3d.triturus.gisimplm.GmSimpleTINFeature;
  * exceed the value {@link this#nMax}. For <i>n</i> &lt; 1 or <i>n</i> &gt; 
  * {@link this#nMax}, the {@link this#transform} method will throw an 
  * exception.
- * <br/>
- * TODO: This method has not been implemented yet!
  * 
- * @author Benno Schmidt
+ * @author Maximilian Haverkamp, Benno Schmidt
  */
 public class FltTINCorrelation extends T3dProcFilter
 {
     private String logString = "";
 
     /**
+     * @deprecated
 	 * maximum size of the moving computation window is (2<i>nMax</i> + 1) x 
 	 * (2<i>nMax</i> + 1).
 	 */
-	public short nMax = 6;
+	public short nMax = 6; 
 	private short n = 3; // computation window size will be (2n + 1) x (2n + 1)
-	
+	private int c = 500;
 	private GmSimpleTINFeature tin1, tin2; // Operation inputs
-
+	
+	private String z = "max";
+	private String u = "ARC_INFO_ASCII_GRID";
+	private String s = "Quadrat";
+	
 	
     public FltTINCorrelation() {
         logString = this.getClass().getName();
@@ -102,16 +112,26 @@ public class FltTINCorrelation extends T3dProcFilter
      * @param n Windows size parameter
      */
     public void setWindowSize(short n) {
-    	checkWindowSize(n);
+    	// checkWindowSize(n);
     	this.n = n;
 	}
 
+    public short getWindowSize() {
+		return n;
+	}
+    
+    /**
+     * @deprecated Method not needed anymore, any odd number works
+     * @param n
+     */
+    /*
 	private void checkWindowSize(short n) {
 		if (n > nMax)
     		throw new T3dException(
     			"Invalid computation window size " + 
     			"(must be < " + (nMax + 1) + ")!");
 	}
+	*/
 
     /**
      * returns the available window size names.
@@ -128,7 +148,7 @@ public class FltTINCorrelation extends T3dProcFilter
    
     // TODO Window stuff is redundant -> refactor!
     
-    // TODO Rasterizer-Parameter setzen! Hier: CellSize -> Rückgabe: Resultierende #Gitterzellen
+    // TODO Rasterizer-Parameter setzen! Hier: CellSize -> Rueckgabe: Resultierende #Gitterzellen
 
     /**
      * returns options to choose a reasonable rasterization cell-size.
@@ -137,31 +157,67 @@ public class FltTINCorrelation extends T3dProcFilter
      */
     public String[] UIDialog_CellSizeOptions() {
     	Double dx = this.reasonableCellSize();
-    	String[] res = new String[3];
+    	// original Benno:
+    	/*
+      	String[] res = new String[3];
     	res[0] = "" + Math.round(dx * 2.0) + " (fast computation)";
     	res[1] = "" + Math.round(dx);
     	res[2] = "" + Math.round(dx / 2.0) + " (intensive computation)";
+    	*/
+    	// new Maximilian:
+    	String[] res = new String[6];
+    	res[0] = "" + Math.round(dx * 2.0);
+    	res[1] = "" + Math.round(750);
+    	res[2] = "" + Math.round(dx / 2.0);
+    	res[3] = "" + Math.round(250);
+    	res[4] = "" + Math.round(100);
+    	res[5] = "" + Math.round(50);
+
     	return res;
     }
 
 	private Double reasonableCellSize() {
-		// TODO vernünftige Logik implementieren!
+		// TODO vernuenftige Logik implementieren!
 		return 500.0;
 	}
 
+	public void setCellSize(int c) {
+		this.c = c;
+	}
+
+	public int getCellSize() {
+		// TODO Auto-generated method stub
+		return c;
+	}
+	
+	public String getWindowForm(){
+		return s;
+	}
+	
+	public void setWindowForm(String s){
+		this.s = s;
+	}
+		
     /**
      * returns available output format options.
      * 
      * @return Array holding list entry titles 
      */
     public String[] UIDialog_OutputFormatOptions() {
-    	String[] res = new String[3];
-    	res[0] = "ESRI Shapefile";
+    	String[] res = new String[2];
+    	res[0] = "VTK Dataset File";
     	res[1] = "Arc/Info ASCII Grid";
-    	res[2] = "VTK Dataset File";
+    	//res[2] = "ESRI Shapefile";
     	return res;
     }
 
+    public String[] UIDialog_WindowformOptions() {
+    	String[] res = new String[2];
+    	res[0] = "Quadrat";
+    	res[1] = "Kreis";
+    	return res;
+    }
+    
     /**
      * returns available z-conflict handling options.
      * 
@@ -175,25 +231,339 @@ public class FltTINCorrelation extends T3dProcFilter
     	return res;
     }
 
+    public void setZConflictHandler(String z) {
+		this.z = z;
+    }
+    
+	public String getZConflictHandler() {
+		return z;
+	}
+	
+	public void setOutputFormat(String outputFormat) {
+		this.u = outputFormat;
+
+	}
+	public String getOutputFormat() {
+		//u= "IoFormatType."+u;
+		//System.out.println(u);
+		return u;
+	}
+
 	/** 
-     * calculates the correlation coefficient of two elevation-grids.
+     * computes the correlation coefficient of two elevation-grids.
      *
      * @param tin1 First input TIN
      * @param tin2 Second input TIN
+     * @param cellSize
+	 * @param windowSize
      * @return Result grid
      * @throws T3dException
      */
-    public GmSimpleElevationGrid transform(
-    	GmSimpleTINFeature tin1, GmSimpleTINFeature tin2) 
+    public VgElevationGrid  transform(
+    	GmSimpleTINFeature tin1, GmSimpleTINFeature tin2,
+    	short windowSize, int cellSize, String zConflictHandler, String windowForm) 
     	throws T3dException
     {
     	if (tin1 != null) this.tin1 = tin1;
     	if (tin2 != null) this.tin2 = tin2;
     	
-        this.checkWindowSize(n);
+        // this.checkWindowSize(n);
         
-        throw new T3dNotYetImplException();
-        
-        //return null;
-    }    
+		try {
+			// creating the Bounding Box for the area
+			VgEnvelope bbox = GmEnvelope.intersect(tin1.envelope(), tin2.envelope());
+
+
+			VgEquidistGrid grdGeom = this.setUpGeometry(bbox, cellSize);
+
+			// creating grids
+			FltTIN2ElevationGrid gridOne = new FltTIN2ElevationGrid();
+			FltTIN2ElevationGrid gridTwo = new FltTIN2ElevationGrid();
+			FltTIN2ElevationGrid correlation = new FltTIN2ElevationGrid();
+			//setting the conflict handler for occurance of multiple Z Coordinates
+			if(zConflictHandler=="max") {
+				gridOne.setZConflictHandler(FltTIN2ElevationGrid.CONFLICT_TAKE_MAX_Z);
+				gridTwo.setZConflictHandler(FltTIN2ElevationGrid.CONFLICT_TAKE_MAX_Z);
+			} else if(zConflictHandler=="min") {
+				gridOne.setZConflictHandler(FltTIN2ElevationGrid.CONFLICT_TAKE_MIN_Z);
+				gridTwo.setZConflictHandler(FltTIN2ElevationGrid.CONFLICT_TAKE_MIN_Z);
+			} else if(zConflictHandler=="avg") {
+				gridOne.setZConflictHandler(FltTIN2ElevationGrid.CONFLICT_TAKE_AVG_Z);
+				gridTwo.setZConflictHandler(FltTIN2ElevationGrid.CONFLICT_TAKE_AVG_Z);
+			}
+
+
+
+			gridOne.setGridGeometry(grdGeom);
+			gridTwo.setGridGeometry(grdGeom);
+			correlation.setGridGeometry(grdGeom);
+
+			VgElevationGrid grd = gridOne.transform(tin1);
+			VgElevationGrid grd2 = gridTwo.transform(tin2);
+			VgElevationGrid grdCorr = correlation.transform(tin1);
+
+
+			double[][] grdOneDeviation = new double[grd.numberOfRows()][grd.numberOfColumns()];
+			double[][] grdTwoDeviation = new double[grd2.numberOfRows()][grd2.numberOfColumns()];
+			double[][] covariance = new double[grd2.numberOfRows()][grd2.numberOfColumns()];
+
+			switch (windowForm){
+
+			case ("Quadrat"):
+
+
+			//first entry is set with top left window corner on top left grid corner
+
+			for (int i = 0 + ((windowSize - 1) / 2); i < grd.numberOfRows() - ((windowSize + 1) / 2); i++) {
+
+				for (int j = 0 + ((windowSize - 1) / 2); j < grd.numberOfColumns() - ((windowSize + 1) / 2); j++) {
+					double meanGridOne = 0;
+					double sumX = 0;
+					double meanGridOne2 = 0;
+					double sumY = 0;
+					double sumXY = 0;
+					int counter = 0; // auxiliary variable
+
+					{
+						// checking if every field in the current window position is set, otherwise the correlation
+						// would be irregular
+						for (int k = -((windowSize - 1) / 2); k < (windowSize + 1) / 2; k++) {
+							for (int l = -((windowSize - 1) / 2); l < (windowSize + 1) / 2; l++) {
+
+
+								if ((boolean) ((GmSimpleElevationGrid) grd2).isSet(i + k, j + l) != false
+										&& (boolean) ((GmSimpleElevationGrid) grd).isSet(i + k, j + l) != false) {
+
+									counter++;
+								}
+
+
+
+							}
+						}
+						// calculation means of the current window
+						if (counter == (windowSize * windowSize)) { // -1
+							for (int k = -((windowSize - 1) / 2); k < (windowSize + 1) / 2; k++) {
+								for (int l = -((windowSize - 1) / 2); l < (windowSize + 1) / 2; l++) {
+
+									meanGridOne = meanGridOne + (((GmSimpleElevationGrid) grd).getValue(i + k, j + l));
+									meanGridOne2 = meanGridOne2
+											+ (((GmSimpleElevationGrid) grd2).getValue(i + k, j + l));
+
+
+
+								}
+							}
+
+							meanGridOne = meanGridOne / (windowSize * windowSize);
+							meanGridOne2 = meanGridOne2 / (windowSize * windowSize);
+							//calculating sums in each window
+							for (int k = -((windowSize - 1) / 2); k < (windowSize + 1) / 2; k++) {
+								for (int l = -((windowSize - 1) / 2); l < (windowSize + 1) / 2; l++) {
+
+
+									sumX = sumX + Math
+											.pow(((GmSimpleElevationGrid) grd).getValue(i + k, j + l) - meanGridOne, 2);
+									sumY = sumY + Math.pow(
+											((GmSimpleElevationGrid) grd2).getValue(i + k, j + l) - meanGridOne2, 2);
+									sumXY = sumXY + (((GmSimpleElevationGrid) grd).getValue(i + k, j + l)
+											- meanGridOne)
+											* (((GmSimpleElevationGrid) grd2).getValue(i + k, j + l) - meanGridOne2);
+
+								}
+							}
+						}
+					}
+					grdOneDeviation[i][j] = sumX;
+
+					grdTwoDeviation[i][j] = sumY;
+					covariance[i][j] = sumXY;
+
+				}
+			}break;
+			case("Kreis"):
+				int circleCheck=0;
+			for (int z=0; z<=(windowSize-1)/2;z++){
+				circleCheck=circleCheck+z;
+			}
+			circleCheck=windowSize*windowSize-4*circleCheck; //Dreieckszahlen
+			System.out.println("CIRCLECHECK: "+circleCheck);
+			//first entry is set with top left window corner on top left grid corner
+
+			for (int i = 0 + ((windowSize - 1) / 2); i < grd.numberOfRows() - ((windowSize + 1) / 2); i++) {
+
+				for (int j = 0 + ((windowSize - 1) / 2); j < grd.numberOfColumns() - ((windowSize + 1) / 2); j++) {
+					double meanGridOne = 0;
+					double sumX = 0;
+					double meanGridTwo = 0;
+					double sumY = 0;
+					double sumXY = 0;
+					int counter = 0; // auxiliary variable
+
+					int x=((windowSize - 1) / 2)+1;
+					int y=((windowSize - 1) / 2)+1;
+						// checking if every field in the current window position is set, otherwise the correlation
+						// would be irregular
+					for (int k = -(windowSize-1)/2; k<(windowSize+1)/2; k++) {
+							if(y>0) {
+								x--;
+							}else{
+								x++;
+							}
+							y--;
+
+							for (int l = -((windowSize - 1) / 2)+x; l <= ((windowSize - 1) / 2)-x;l++) {
+
+
+								if ((boolean) ((GmSimpleElevationGrid) grd2).isSet(i + k, j + l) != false
+										&& (boolean) ((GmSimpleElevationGrid) grd).isSet(i + k, j + l) != false) {
+
+									counter++;
+
+								}
+
+
+
+							}
+						}
+						// calculation means of the current window
+
+
+						if (counter == circleCheck) {
+
+							x=((windowSize - 1) / 2)+1;
+							y=((windowSize - 1) / 2)+1;
+
+							for (int k = -(windowSize-1)/2; k<(windowSize+1)/2; k++) {
+								if(y>0) {
+									x--;
+								}else{
+									x++;
+								}
+								y--;
+								for (int l = -((windowSize - 1) / 2)+x; l <= ((windowSize - 1) / 2)-x;l++) {
+									meanGridOne = meanGridOne + (((GmSimpleElevationGrid) grd).getValue(i + k, j + l));
+									meanGridTwo = meanGridTwo
+											+ (((GmSimpleElevationGrid) grd2).getValue(i + k, j + l));
+
+
+
+
+								}
+
+
+							}
+
+
+
+							meanGridOne = meanGridOne / (circleCheck);
+							meanGridTwo = meanGridTwo / (circleCheck);
+							//calculating sums in each window
+							x=((windowSize - 1) / 2)+1;
+							y=((windowSize - 1) / 2)+1;
+							for (int k = -(windowSize-1)/2; k<(windowSize+1)/2; k++) {
+								if(y>0) {
+									x--;
+								}else{
+									x++;
+								}
+								y--;
+								for (int l = -((windowSize - 1) / 2)+x; l <= ((windowSize - 1) / 2)-x; l++) {
+
+
+
+									sumX = sumX + Math
+											.pow(((GmSimpleElevationGrid) grd).getValue(i + k, j + l) - meanGridOne, 2);
+									sumY = sumY + Math.pow(
+											((GmSimpleElevationGrid) grd2).getValue(i + k, j + l) - meanGridTwo, 2);
+									sumXY = sumXY + (((GmSimpleElevationGrid) grd).getValue(i + k, j + l)
+											- meanGridOne)
+											* (((GmSimpleElevationGrid) grd2).getValue(i + k, j + l) - meanGridTwo);
+
+
+								}
+							}
+
+					    }
+
+					grdOneDeviation[i][j] = sumX;
+
+					grdTwoDeviation[i][j] = sumY;
+					covariance[i][j] = sumXY;
+
+				}
+			}
+
+
+			break;
+			}
+
+			double[][] r = new double[grd2.numberOfRows()][grd2.numberOfColumns()];
+
+			// calculating correlation and setting value
+			for (int i = 0; i < r.length; i++) {
+				for (int j = 0; j < r[0].length; j++) {
+					r[i][j] = covariance[i][j] / (Math.sqrt(grdOneDeviation[i][j]) * Math.sqrt(grdTwoDeviation[i][j]));
+
+					if (r[i][j] > 1 || r[i][j] < -1) { // Check if the calculations are correct
+						//System.out.println(r[i][j]);
+						//throw new Error("Error in the calculations");
+						r[i][j]=Math.round(r[i][j]);
+					}
+
+				}
+			}
+
+
+			// unset every not used field for better visualization
+			for (int i = 0; i < (grdCorr.numberOfRows()); i++) {
+				for (int j = 0; j < (grdCorr.numberOfColumns()); j++) {
+
+					((GmSimpleElevationGrid) grdCorr).unset(i, j);
+
+				}
+			}
+
+
+			// Input of correlations
+			for (int i = 0 + ((windowSize - 1) / 2); i < grdCorr.numberOfRows() - ((windowSize + 1) / 2); i++) {
+				for (int j = 0 + ((windowSize - 1) / 2); j < grdCorr.numberOfColumns() - ((windowSize + 1) / 2); j++) {
+					if (!new Double(r[i][j]).isNaN()) { // When Sum of X or Y is zero, the correlation is NaN
+						grdCorr.setValue(i, j, r[i][j]);
+
+
+					}
+
+				}
+
+			}
+
+
+
+			//returning correlation grid
+			return grdCorr;
+
+
+		} catch (T3dException e) {
+			e.printStackTrace();
+		}
+
+        return null;
+    }
+    
+	private VgEquidistGrid setUpGeometry(VgEnvelope bbox, double cellSize) 
+	{
+		System.out.println(bbox);
+		
+		VgPoint origin = new GmPoint(bbox.getXMin(), bbox.getYMin(), 0.0);
+		// TODO: origin ist noch ein schraeger Wert -> ist zu runden gemaess cellSize!
+		
+		int nrows = (int)(Math.floor(bbox.getExtentY() / cellSize)) + 1;
+		int ncols = (int)(Math.floor(bbox.getExtentX() / cellSize)) + 1;
+		        
+		GmSimple2dGridGeometry res = new GmSimple2dGridGeometry(
+			ncols, nrows, origin, cellSize, cellSize);
+		System.out.println(res);
+		return res;
+	}
 }
